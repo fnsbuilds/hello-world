268672bfb8053a9ef5a683115dc7709ab15d6009
Felipe Nascimento <developer.felipens@gmail.com>
2026-04-23 16:12:07 -0300

    Add Fastify and automated tests

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const http = require('http');
+import Fastify from 'fastify';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Felipe\n');
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { message: 'Hello, Felipe' };
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-});
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+export default fastify;
+
+if (process.argv[1] && import.meta.url.endsWith(process.argv[1].split('/').pop())) {
+  start();
+}
